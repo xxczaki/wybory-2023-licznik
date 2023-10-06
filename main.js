207deda46b1b221d3fb59b0b7fbe9e2e94ee3b13
tomasz-sacharczuk <82565333+tomasz-sacharczuk@users.noreply.github.com>
2023-10-06 13:59:10 +0200

    Update main.js
    trochę DRY

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 import './style.css';
-
 import { registerSW } from 'virtual:pwa-register'
+
+function updateCounter(id, operation) {
+	const countElement = document.getElementById(id);
+	const rawCount = localStorage.getItem(id) ?? '0';
+	let convertedCount = Number.parseInt(rawCount, 10);
+
+	switch (operation) {
+		case 'add':
+			convertedCount++;
+			break;
+		case 'sub':
+			if (convertedCount === 0) break;
+			convertedCount--;
+			break;
+	}
+
+	localStorage.setItem(id, convertedCount.toString());
+	countElement.textContent = convertedCount.toString();
+}
 
 registerSW({ 
 	immediate: true,
@@ -14,129 +32,26 @@ if (localStorage.getItem('offline-ready') === 'true') {
 	document.getElementById('offline-info').textContent = '✅ Gotowość do działania off-line';
 }
 
-// Głosujący
-const GLOSUJACY_COUNT_ID = 'glosujacy-count';
+const categories = [
+	'glosujacy',
+	'sejm',
+	'senat',
+	'referendum',
+];
 
-function updateVisibleGlosujacyCount(operation) {
-	const rawCount = localStorage.getItem(GLOSUJACY_COUNT_ID) ?? '0';
-	let convertedCount = Number.parseInt(rawCount, 10);
+categories.forEach(category => {
+	const countId = `${category}-count`;
 
-	switch (operation) {
-		case 'add':
-			convertedCount++;
-			break;
-		case 'sub': {
-			if (convertedCount === 0) break;
+	updateCounter(countId);
 
-			convertedCount--;
-			break;
-		}
-	}
+	document.getElementById(`${category}-add`).onclick = () => {
+		updateCounter(countId, 'add');
+	};
 
-	localStorage.setItem(GLOSUJACY_COUNT_ID, convertedCount.toString());
-	document.getElementById(GLOSUJACY_COUNT_ID).textContent = convertedCount.toString();
-}
-
-updateVisibleGlosujacyCount();
-
-document.getElementById('glosujacy-add').onclick = () => {
-	updateVisibleGlosujacyCount('add');
-};
-
-document.getElementById('glosujacy-sub').onclick = () => {
-	updateVisibleGlosujacyCount('sub');
-};
-
-// Sejm
-const SEJM_COUNT_ID = 'sejm-count';
-
-function updateVisibleSejmCount(operation) {
-	const rawCount = localStorage.getItem(SEJM_COUNT_ID) ?? '0';
-	let convertedCount = Number.parseInt(rawCount, 10);
-
-	switch (operation) {
-		case 'add':
-			convertedCount++;
-			break;
-		case 'sub': {
-			if (convertedCount === 0) break;
-
-			convertedCount--;
-			break;
-		}
-	}
-
-	localStorage.setItem(SEJM_COUNT_ID, convertedCount.toString());
-	document.getElementById(SEJM_COUNT_ID).textContent = convertedCount.toString();
-}
-
-updateVisibleSejmCount();
-
-document.getElementById('sejm-add').onclick = () => {
-	updateVisibleSejmCount('add');
-};
-
-document.getElementById('sejm-sub').onclick = () => {
-	updateVisibleSejmCount('sub');
-};
-
-// Senat
-const SENAT_COUNT_ID = 'senat-count';
-
-function updateVisibleSenatCount(operation) {
-	const rawCount = localStorage.getItem(SENAT_COUNT_ID) ?? '0';
-	let convertedCount = Number.parseInt(rawCount, 10);
-
-	switch (operation) {
-		case 'add':
-			convertedCount++;
-			break;
-		case 'sub': {
-			if (convertedCount === 0) break;
-
-			convertedCount--;
-			break;
-		}
-	}
-
-	localStorage.setItem(SENAT_COUNT_ID, convertedCount.toString());
-	document.getElementById(SENAT_COUNT_ID).textContent = convertedCount.toString();
-}
-
-updateVisibleSenatCount();
-
-document.getElementById('senat-add').onclick = () => {
-	updateVisibleSenatCount('add');
-};
-
-document.getElementById('senat-sub').onclick = () => {
-	updateVisibleSenatCount('sub');
-};
-
-// Referendum
-const REFERENDUM_COUNT_ID = 'referendum-count';
-
-function updateVisibleReferendumCount(operation) {
-	const rawCount = localStorage.getItem(REFERENDUM_COUNT_ID) ?? '0';
-	let convertedCount = Number.parseInt(rawCount, 10);
-
-	switch (operation) {
-		case 'add':
-			convertedCount++;
-			break;
-		case 'sub': {
-			if (convertedCount === 0) break;
-
-			convertedCount--;
-			break;
-		}
-	}
-
-	localStorage.setItem(REFERENDUM_COUNT_ID, convertedCount.toString());
-	document.getElementById(REFERENDUM_COUNT_ID).textContent = convertedCount.toString();
-}
-
-updateVisibleReferendumCount();
+	document.getElementById(`${category}-sub`).onclick = () => {
+		updateCounter(countId, 'sub');
+	};
+});
 
 document.getElementById('referendum-add').onclick = () => {
 	updateVisibleReferendumCount('add');
